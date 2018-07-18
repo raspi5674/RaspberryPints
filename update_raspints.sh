ba85b1c4a125e9670d9ff6318eade9cc8d666a4a
raspi5674 <34964197+raspi5674@users.noreply.github.com>
2018-07-18 13:06:43 -0500

    mv to cp

--- update_raspints.sh
+++ update_raspints.sh
@@ -1,4 +1,4 @@
 # This update pulls the most recent version of raspberry pints and then updates it
 cd ~/git/RaspberryPints
 git pull origin
-mv ~/git/RaspberryPints/index_2.php /var/www/index.php
+cp ~/git/RaspberryPints/index_2.php /var/www/index.php
